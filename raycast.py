9ae6deb6456b45845e4e13cfa0b5b0b2ed2945e7
netanel <netanel.attali@gmail.com>
2022-10-05 01:34:23 +0300

    2d view working

--- raycast.py
+++ raycast.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import math
 
 
 class Player:
@@ -7,12 +8,59 @@ class Player:
         self.color = "yellow"
         self.size = 10
         self.rect = Rect([x - self.size // 2, y - self.size // 2, self.size, self.size])
+        self.rotation = 45
+        self.dx = math.cos(math.radians(self.rotation))
+        # dy is negative sin since screen y coordinates increase downward, unlike Cartesian
+        self.dy = -math.sin(math.radians(self.rotation))
+        self.view_line = self._calc_view_line()
 
     def draw(self, surface):
         pygame.draw.rect(surface, self.color, self.rect)
+        pygame.draw.line(surface, self.color, self.rect.center, self.view_line, 5)
 
-    def move(self, x=0, y=0):
-        self.rect = self.rect.move(x, y)
+    def move(self, d):
+        speed = 5
+        if d > 0:
+            x = self.dx
+            y = self.dy
+        else:
+            x = -self.dx
+            y = -self.dy
+        self.rect = self.rect.move(x * speed, y * speed)
+        self.view_line = self._calc_view_line()
+
+    def rotate(self, n):
+        speed = 2
+        self.rotation = (self.rotation + n * speed) % 360
+        # print(self.rotation)
+        self.dx = math.cos(math.radians(self.rotation))
+        self.dy = -math.sin(math.radians(self.rotation))
+        self.view_line = self._calc_view_line()
+
+    def _calc_view_line(self):
+        line_length = 50
+        dx = self.rect.centerx + self.dx * line_length
+        dy = self.rect.centery + self.dy * line_length
+        # print(dx, dy, self.rotation)
+
+        return dx, dy
+
+    def strafe(self, d):
+
+        speed = 2
+        if d == 'left':
+            angle = 90
+        else:
+            angle = -90
+        x = math.cos(math.radians(self.rotation + angle))
+        y = -math.sin(math.radians(self.rotation + angle))
+        # print("rotation ", self.rotation, self.rotation+angle)
+        print("pos ", self.rect.center)
+        print("move by ", x * speed, y * speed)
+
+        self.rect = self.rect.move(round(x * speed), round(y * speed))
+        print("new pos ", self.rect.center)
+        self.view_line = self._calc_view_line()
 
 
 class Map:
@@ -28,7 +76,7 @@ class Map:
         for row_idx, row in enumerate(self.map):
             for col_idx, cell in enumerate(row):
                 rect_map[row_idx][col_idx] = Rect(
-                    [col_idx * self.cube_size, row_idx * self.cube_size, self.cube_size-1, self.cube_size-1])
+                    [col_idx * self.cube_size, row_idx * self.cube_size, self.cube_size - 1, self.cube_size - 1])
         return rect_map
 
     def draw(self, surface):
@@ -43,7 +91,7 @@ class App:
         self.screen = None
         self._caption = caption
         self.size = self.width, self.height = width, height
-        self.player = Player(width // 2, height // 2)
+        self.player = Player(width // 3, height // 2)
         self.clock = pygame.time.Clock()
         self.fps = 60
         self.map = Map([
@@ -69,13 +117,19 @@ class App:
     def on_loop(self):
         keys = pygame.key.get_pressed()
         if keys[K_LEFT]:
-            self.player.move(x=-5)
+            if keys[K_LALT]:
+                self.player.strafe('left')
+            else:
+                self.player.rotate(+1)
         if keys[K_RIGHT]:
-            self.player.move(x=5)
+            if keys[K_LALT]:
+                self.player.strafe('right')
+            else:
+                self.player.rotate(-1)
         if keys[K_UP]:
-            self.player.move(y=-5)
+            self.player.move(+1)
         if keys[K_DOWN]:
-            self.player.move(y=5)
+            self.player.move(-1)
 
     def on_render(self):
         self.screen.fill("gray")
